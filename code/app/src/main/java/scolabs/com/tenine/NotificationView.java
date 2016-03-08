e1397d97e03110e5670c458e29ae169a1e963653
scolary20 <juniorscolary2000@yahoo.fr>
2016-03-08 04:44:38 +0200

    service_opt

---
+++ code/app/src/main/java/scolabs/com/tenine/NotificationView.java
@@ -0,0 +1,38 @@
+package scolabs.com.tenine;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.widget.TextView;
+
+public class NotificationView extends Activity {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.f);
+            CharSequence s = "Inside the activity of Notification one ";
+            int id=0;
+
+            Bundle extras = getIntent().getExtras();
+            if (extras == null) {
+                s = "error";
+            }
+            else {
+                id = extras.getInt("notificationId");
+            }
+            TextView t = (TextView) findViewById(R.id.text2);
+            s = s+"with id = "+id;
+            t.setText(s);
+            NotificationManager myNotificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+            // remove the notification with the specific id
+            myNotificationManager.cancel(id);
+            Intent i = new Intent(NotificationView.this, Login.class);
+            startActivity(i);
+        }
+}
